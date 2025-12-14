2b3d9951cf23a7e555eceee324ed88b75c2c1812
michaelpme <michael.p.mekonnen@gmail.com>
2025-12-14 10:38:12 -0500

    commented the js and css files

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,72 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //assigns the element in our document with the class fa-trash to deleteBtn variable. querySelectorAll returns a node list
+const item = document.querySelectorAll('.item span') // assigns the span element with the class item and the  to the item variable. querySelectorAll returns a node list
+const itemCompleted = document.querySelectorAll('.item span.completed') // assigns the  element with the class completed that has a span with class item, querySelectorAll returns a node list
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+Array.from(deleteBtn).forEach((element)=>{ // since it deleteBtn is a nodeList we must use the Array method to make use of it. each element of the array is iterated over and has an event listner added to it
+    element.addEventListener('click', deleteItem) // adding an event listner to each element of the array
 })
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+Array.from(item).forEach((element)=>{ // since it item is a nodeList we must use the Array method to make use of it as a normal array. each element of the array is iterated over and has an event listner added to it
+    element.addEventListener('click', markComplete)// adding an event listner to each element of the array
 })
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+Array.from(itemCompleted).forEach((element)=>{  // since it itemCompleted is a nodeList we must use the Array method to make use of it as a normal array. each element of the array is iterated over and has an event listner added to it
+    element.addEventListener('click', markUnComplete)// adding an event listner to each element of the array
 })
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteItem(){ // start of an async function 
+    const itemText = this.parentNode.childNodes[1].innerText // it takes the item from the DOM that triggered the function, goes to it's parent node and retieves the first child's inner text
     try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
+        const response = await fetch('deleteItem', { //selecting the route where request goes to 
+            method: 'delete', // the type action to be performed 
+            headers: {'Content-Type': 'application/json'}, // the kind of data we will be sending
+            body: JSON.stringify({//turnining the object into a json formart
+              'itemFromJS': itemText // information pulled from the DOM (variable set on line 18) and put into an object
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() // once the fetch await is complete we set the data variable with a json response
+        console.log(data) // console log the data variable
+        location.reload() // and finally reload the page
 
     }catch(err){
-        console.log(err)
+        console.log(err) // catching any errors that may occur
     }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function markComplete(){ //start of an async function that will be triggered when the items are marked as complete
+    const itemText = this.parentNode.childNodes[1].innerText // it takes the item from the DOM that triggered the function, goes to it's parent node and retieves the first child's inner text
     try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
+        const response = await fetch('markComplete', {//selecting the route where request goes to 
+            method: 'put',// the type action to be performed 
+            headers: {'Content-Type': 'application/json'},// the kind of data we will be sending
+            body: JSON.stringify({//turnining the object into a json formart
+                'itemFromJS': itemText// information pulled from the DOM (variable set on line 18) and put into an object
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json()// once the fetch await is complete we set the data variable with a json response
+        console.log(data)// console log the data variable
+        location.reload()// and finally reload the page
 
     }catch(err){
-        console.log(err)
+        console.log(err)// catching any errors that may occur
     }
 }
 
 async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
+    const itemText = this.parentNode.childNodes[1].innerText // it takes the item from the DOM that triggered the function, goes to it's parent node and retieves the first child's inner text
     try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
+        const response = await fetch('markUnComplete', {//selecting the route where request goes to 
+            method: 'put',// the type action to be performed 
+            headers: {'Content-Type': 'application/json'},// the kind of data we will be sending
+            body: JSON.stringify({//turnining the object into a json formart
+                'itemFromJS': itemText// information pulled from the DOM (variable set on line 18) and put into an object
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json()// once the fetch await is complete we set the data variable with a json response
+        console.log(data)// console log the data variable
+        location.reload()// and finally reload the page
 
     }catch(err){
-        console.log(err)
+        console.log(err)// catching any errors that may occur
     }
 }
